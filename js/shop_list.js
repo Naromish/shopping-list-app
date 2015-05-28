b45537e28c2ad0f56739c52c76606c4d11005b6e
Naromish <danermc@gmail.com>
2015-05-27 22:18:55 -0400

    sort and delete

--- js/shop_list.js
+++ js/shop_list.js
@@ -1,58 +1,48 @@
-function addListItem() {
-    var text = $("#add-item").val();
-    $("#add").append('<li>' + text + '</li>');
-    $("#add-item").val('');
-
-};
-
-
 $(document).ready(function() {
-    $("#new").on('click', addListItem);
-    $('#add').on('dblclick', 'li', function() {
+    $('.add').on('dblclick', 'li', function() {
         $(this).toggleClass('strike');
         $(this).children().toggleClass();
     });
 
-    
-    //the below is not working and the delete icon is large and in the wrong position//
-
-    $('#add').on('hover', 'li', function() {
-        $(this).toggleClass('delete');
-        $(this).children().toggleClass();
+    $('.add').on('click', '.delete', function(e){
+            e.preventDefault(); 
+            $(this).parent().remove();
     });
-    
+    $('.add').sortable({ axis: "y" });
+});
 
-    //the below is not working//
+//figure out to hide delete img so it doesn't enlarge when strike
 
-    $('add-item').keypress(function(e) {
-        if (e.keyCode == 13) {
-            alert('Entered');
-        }
-    
-
+    //don't think i need the below anymore;found different solution
+    //$('.add').on('hover', '.items', function() {
+     //   $('.delete').toggleClass('show');
+    //});
 
 
+$(document).keydown(function(e) {
+    if (e.keyCode == 13) {
+         var text = $(".add-item").val();
+        $(".add").append("<li>" + text + "<img class='delete' src='images/circle_x.png'></li>");
+       $(".add-item").val('');
+    }
+});
+
+
+//figure out why the delete button doesn't show consistently; something's off
+
+$(document).mousedown(function(e) {
+
+    $('.add').on('mouseenter', 'li', function(){
+        $(this).children('.delete').toggleClass('show');
     });
+    $('.add').on('mouseleave', 'li', function(){
+        $(this).children('.delete').toggleClass('show');
+    });
+});
 
 
-   });
-
-
-
- 
-
-
-
-
-
-
-//want to remove ADD button and have input by ENTER key only
-//right now only the first input field works now, how do I get the rest to work?
-
-//how can i properly display the delete icon? I want it to appear on the right of the list item when hovering; then clicking the icon will delete that list item
-//right now the icon is very big and displaying in the background
+//how to create multiple inputs/categories; different divs?
 
 //how do I create a functional button at bottom to clear list?
 //how do I style that button?
 
-//still need to figure out how to make sortable the categories (along with any list items underneath)
